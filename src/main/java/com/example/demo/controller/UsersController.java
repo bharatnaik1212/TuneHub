767762a17d65e15f431c07b16f293c60a63d2f78
Bharat Naik <Bharat Naik@DESKTOP-GARLJAI>
2024-01-13 14:09:52 +0530

    TuneHub

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,16 +1,20 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.entities.Song;
 import com.example.demo.entities.Users;
+import com.example.demo.services.SongService;
 import com.example.demo.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -20,6 +24,9 @@ public class UsersController
 {
 	@Autowired
 	UsersService service;
+	
+	@Autowired
+	SongService songService;
 	
 	@PostMapping("/register")
 	public String addUsers(@ModelAttribute Users user) {
@@ -40,7 +47,7 @@ public class UsersController
 	@PostMapping("/validate")
 	public String validate(@RequestParam ("email") String email, 
 			               @RequestParam ("password") String password,
-			               HttpSession session)
+			               HttpSession session,Model model)
 	{
 		if(service.validateUser(email,password)==true)
 		{
@@ -50,10 +57,17 @@ public class UsersController
 			
 			if(role.equals("admin"))
 			{
-				return "adminHome";
+			return "adminHome";
 			}
 			else
 			{
+				Users user=service.getUser(email);
+				boolean userStatus=user.isPremium();
+				 List<Song> songsList=songService.fetchAllSongs();
+			 		model.addAttribute("songs", songsList);
+
+				model.addAttribute("isPremium",userStatus);
+				
 				return "customerHome";
 			}	
 		}
